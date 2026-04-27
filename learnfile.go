2f483b9e7dca383f001a1a89b624e3cbc28f31f5
Gbenga Jolayemi <gbenga.jolayemi@2am.ng>
2026-04-27 15:32:03 +0100

    fix

--- learnfile.go
+++ learnfile.go
@@ -1,15 +1,18 @@
 package main
-func main() {
-   day := 5
 
-   switch day {
-   case 1,3,5:
-    fmt.Println("Odd weekday")
-   case 2,4:
-     fmt.Println("Even weekday")
-   case 6,7:
-    fmt.Println("Weekend")
-  default:
-    fmt.Println("Invalid day of day number")
-  }
+import "fmt"
+
+func main() {
+	day := 5
+
+	switch day {
+	case 1, 3, 5:
+		fmt.Println("Odd weekday")
+	case 2, 4:
+		fmt.Println("Even weekday")
+	case 6, 7:
+		fmt.Println("Weekend")
+	default:
+		fmt.Println("Invalid day of day number")
+	}
 }
